46b157b4c904e2fa6236ac05c0ef0c809be0cc5e
Tawanda Msengezi <xrthemc@gmail.com>
2021-04-02 08:48:37 +0300

    Updated assembly info

--- Movie Rental/Properties/AssemblyInfo.cs
+++ Movie Rental/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Movie_Rental")]
+[assembly: AssemblyTitle("Special_Sounds")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Movie_Rental")]
+[assembly: AssemblyProduct("Special_Sounds")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
